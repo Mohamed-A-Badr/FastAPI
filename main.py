c756d4084a1f88e818b3739504fecec2df5bbd45
Mohamed Badr <60277974+Mohamed-A-Badr@users.noreply.github.com>
2022-12-01 18:33:05 +0200

    Creating the models and DB as List

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+from typing import List
+from uuid import uuid4
 from fastapi import FastAPI
+from models import Gender, Role, User
 
 app = FastAPI()
+
+db: List[User] = [
+    User(
+        # ? This line makes our id change every time
+        id=uuid4(),  # ? we restart our application
+        first_name="Mohamed",
+        last_name="Badr",
+        gender=Gender.male,
+        roles=[Role.student]
+    ),
+    User(
+        id=uuid4(),
+        first_name="habiba",
+        last_name="Mohamed",
+        gender=Gender.female,
+        roles=[Role.admin, Role.user]
+    ),
+]
 
 
 @app.get("/")
